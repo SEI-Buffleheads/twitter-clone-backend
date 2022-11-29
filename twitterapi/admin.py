64fefd55aff02cdad5c80243f3e435631871b9bc
Tony Wu <tonywu@Tonys-MacBook-Air-2.local>
2022-11-29 12:11:34 -0500

    added runtime

--- twitterapi/admin.py
+++ twitterapi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from authentication.models import User
 
 admin.site.register(Post)
+admin.site.register(Comment)
 admin.site.register(User)
