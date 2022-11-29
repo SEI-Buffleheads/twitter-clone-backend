d5b70ed844e68569a4c4c0fa02a38615f570a957
Tony Wu <tonywu@Tonys-MacBook-Air-2.local>
2022-11-29 13:12:55 -0500

    added files for railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'twitterbackend.wsgi'
--- twitterbackend/settings.py
+++ twitterbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n62zn8(mw%!jy&l9yukjy0jn8uk=v%!0_!#)s$(2o=+uen=079
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOST = ['*']
 
 
 # Application definition
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
