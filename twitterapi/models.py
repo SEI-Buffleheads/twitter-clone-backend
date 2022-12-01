fad8764655e6b0c66035a938c84cf16976dc5223
Tony <76459334+tonywuhoo@users.noreply.github.com>
2022-12-01 13:37:11 -0500

    Update models.py

--- twitterapi/models.py
+++ twitterapi/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Post(models.Model):
   owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="post_owner", null=True)
   text = models.CharField(max_length=128, blank=False)
-  title = models.CharField(max_length=128, default="Text")
+  title = models.CharField(max_length=1000, default="Text")
   date = models.DateTimeField(auto_now_add = True)
   likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='post_liked', blank=True)
 
